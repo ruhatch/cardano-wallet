a8b410071f295dc5260277494f7546162940d44f
Pawel Jakubas <pawel.jakubas@iohk.io>
2019-05-07 15:25:33 +0200

    Add slot, wallet and utxo schema

--- lib/core/src/Cardano/Wallet/DB/Sqlite.hs
+++ lib/core/src/Cardano/Wallet/DB/Sqlite.hs
@@ -18,16 +18,31 @@ import Cardano.Wallet.Primitive.Types
 import Data.Text
     ( Text )
 import Data.Word
-    ( Word32, Word64 )
+    ( Word16, Word32, Word64 )
 import Database.Persist.TH
 import GHC.Generics
     ( Generic (..) )
 
 share [ mkPersist sqlSettings { mpsPrefixFields = False } , mkMigrate "migrateAll" ] [persistLowerCase|
+Slot
+  epochNumber           Word64                 sql=epoch
+  slotNumber            Word16                 sql=slot
+
+  Primary epochNumber slotNumber
+  deriving Show Generic
+
+Wallet
+  walId                Text                    sql=wallet_id
+  walName              Text                    sql=name
+
+  Primary walId walName
+  deriving Show Generic
+
 TxMeta
   theTxMetaId           Text                   sql=tx_id
-  txMetaWalletId        Text                   sql=wallet_id
+  txMetaWalletId        WalletId               sql=wallet_id
   txMetaDirection       Direction              sql=direction
+  txMetaSlotId          SlotId                 sql=slot_id
   txMetaAmount          Word64                 sql=amount
 
   Primary theTxMetaId txMetaWalletId
@@ -50,5 +65,12 @@ TxOutput
   txOutputAmount        Word64                 sql=amount
 
   Primary txOutputTxId txOutputIndex
+  deriving Show Generic
+
+Utxo
+  txinId                TxInputId              sql=txin_id
+  txoutId               TxOutputId             sql=txout_id
+
+  Primary txinId txoutId
   deriving Show Generic
 |]
